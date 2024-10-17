bd59783ca04b3a07b92f79aec2569103ae48d3ca
AlexC <alexclinton133@gmail.com>
2024-10-17 18:11:35 +0100

    Added Race and Horse classes to represent race details and horse information, including properties and methods to add horses to a race

--- CA1/Horse.cs
+++ CA1/Horse.cs
@@ -2,5 +2,14 @@
 
 public class Horse
 {
-    
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string HorseID { get; set; }
+
+    public Horse(string name, int age, string horseID)
+    {
+        Name = name;
+        Age = age;
+        HorseID = horseID;
+    }
 }
--- CA1/Race.cs
+++ CA1/Race.cs
@@ -2,5 +2,19 @@
 
 public class Race
 {
-    
+    public string Name { get; set; }
+    public string StartTime { get; set; }
+    public List<Horse> Horses { get; set; }
+
+    public Race(string name, string startTime)
+    {
+        Name = name;
+        StartTime = startTime;
+        Horses = new List<Horse>();
+    }
+
+    public void AddHorse(Horse horse)
+    {
+        Horses.Add(horse);
+    }
 }
